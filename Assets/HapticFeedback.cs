aae6620dd21d55bfe3b822fa0b1103845dc7decb
preetishkakkar <preetish.kakkar@gmail.com>
2017-10-07 18:40:26 -0400

    d

---
+++ Assets/HapticFeedback.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HapticFeedback : MonoBehaviour {
+
+    public SteamVR_TrackedController controller;
+    public SteamVR_TrackedObject trackedObject;
+
+    public bool shouldStopHaptic;
+    SteamVR_Controller.Device device;
+
+    public bool toggleHapticWhenTriggerPress = false;
+
+    bool invoked = false;
+
+
+    // Use this for initialization
+    void Start () {
+        if(controller == null)
+        {
+            controller = GetComponent<SteamVR_TrackedController>();
+        }
+        if (trackedObject == null)
+        {
+            trackedObject = GetComponent<SteamVR_TrackedObject>();
+        }
+        shouldStopHaptic = false;
+        controller.TriggerClicked += HapticFeedbackProvider;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void LaunchHaptic()
+    {
+        if(device != null)
+            device.TriggerHapticPulse(3000);
+
+        if (shouldStopHaptic)
+        {
+            CancelInvoke();
+            shouldStopHaptic = false;
+        }
+           
+    }
+
+    void HapticFeedbackProvider(object sender, ClickedEventArgs e)
+    {
+        var index = trackedObject.index;
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+        InvokeRepeating("LaunchHaptic", 0.0f, 0.01f);
+        if(invoked && toggleHapticWhenTriggerPress)
+        {
+            shouldStopHaptic = true;
+            invoked = false;
+        } else
+        {
+            invoked = true;
+        }
+        
+    }
+}
