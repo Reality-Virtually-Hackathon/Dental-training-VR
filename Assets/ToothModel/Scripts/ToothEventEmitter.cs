abec9c861dc2a989fbd81de0ada5a85ce96a9936
preetishkakkar <preetish.kakkar@gmail.com>
2017-10-07 23:14:27 -0400

    Tooth thing, working now on desktop!

---
+++ Assets/ToothModel/Scripts/ToothEventEmitter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ToothEventEmitter : MonoBehaviour {
+
+    public Transform tool;
+    public Transform toothToRepair;
+
+    [Header("Event when you are .5 mm far from any mesh")]
+    public UnityEvent closeToMesh;
+
+    [Header("Event when you are .5 mm from given tooth")]
+    public UnityEvent closeToToothToBeRepaired;
+
+    [Header("Event when you are .1 mm or less far from any mesh")]
+    public UnityEvent aboutToHitMesh;
+
+    [Header("Event when you are .1 mm far from any mesh")]
+    public UnityEvent aboutToHitTooth;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
