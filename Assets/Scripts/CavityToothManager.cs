296fa8a16a3fefe0434a8c38ff9f247c4ec38abc
preetishkakkar <preetish.kakkar@gmail.com>
2017-10-07 21:25:59 -0400

    mike's stuff tooth and drill

---
+++ Assets/Scripts/CavityToothManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CavityToothManager : MonoBehaviour {
+
+    Enums.CavityProcedureSteps currentStep;
+
+    [SerializeField]
+    GameObject filling;
+
+	// Use this for initialization
+	void Awake () {
+        currentStep = Enums.CavityProcedureSteps.DrillCavity;
+	}
+
+    public void OnActionCompleted()
+    {
+        switch (currentStep)
+        {
+            case Enums.CavityProcedureSteps.DrillCavity:
+
+                currentStep = Enums.CavityProcedureSteps.FillCavity;
+
+                break;
+            case Enums.CavityProcedureSteps.FillCavity:
+
+                currentStep = Enums.CavityProcedureSteps.PackCavity;
+
+                break;
+            case Enums.CavityProcedureSteps.PackCavity:
+
+                currentStep = Enums.CavityProcedureSteps.ScoopWaste;
+
+                break;
+            case Enums.CavityProcedureSteps.ScoopWaste:
+
+                currentStep = Enums.CavityProcedureSteps.UVLight;
+
+                break;
+            case Enums.CavityProcedureSteps.UVLight:
+                break;
+        }
+    }
+	
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ExcavatorTool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ExcavatorTool : MonoBehaviour, Interfaces.IPickable {
+
+    [SerializeField]
+    private Transform drillBit;
+
+    Rigidbody b;
+
+    IEnumerator inst = null;
+
+    void Awake()
+    {
+        b = GetComponent<Rigidbody>();
+    }
+
+    public void OnDrop()
+    {
+        b.useGravity = true;
+        StopCoroutine(inst);
+        inst = null;
+    }
+
+    public void OnPickup(Transform t, GameObject owner)
+    {
+        Debug.Log("pickingup");
+        b.useGravity = false;
+
+        inst = RigidbodyPosition(t);
+
+        StartCoroutine(inst);
+    }
+
+    IEnumerator RigidbodyPosition(Transform t)
+    {
+        while (true)
+        {
+            b.position = t.position;
+            b.rotation = t.rotation;
+            yield return null;
+        }
+
+    }
+}
